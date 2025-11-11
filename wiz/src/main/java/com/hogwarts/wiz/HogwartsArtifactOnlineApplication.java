127e21380a5eadb0d10fa8d84159ec42c528c1f5
Yuvraj Kalamkar <yuvrajkalamkar96@gmail.com>
2025-11-11 10:36:12 +0530

    Initial commit

---
+++ wiz/src/main/java/com/hogwarts/wiz/HogwartsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.wiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtifactOnlineApplication.class, args);
+	}
+
+}
---
+++ wiz/src/test/java/com/hogwarts/wiz/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogwarts.wiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
