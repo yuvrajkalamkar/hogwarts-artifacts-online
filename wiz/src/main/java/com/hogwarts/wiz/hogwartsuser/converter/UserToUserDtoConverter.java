fee3ea24468130f14134c0938ce7163074729e73
Yuvraj Kalamkar <yuvrajkalamkar96@gmail.com>
2025-11-18 15:02:14 +0530

    User CRUD done! Close #4

---
+++ wiz/src/main/java/com/hogwarts/wiz/hogwartsuser/converter/UserToUserDtoConverter.java
@@ -0,0 +1,22 @@
+package com.hogwarts.wiz.hogwartsuser.converter;
+
+
+import com.hogwarts.wiz.hogwartsuser.HogwartsUser;
+import com.hogwarts.wiz.hogwartsuser.dto.UserDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserToUserDtoConverter implements Converter<HogwartsUser, UserDto> {
+
+    @Override
+    public UserDto convert(HogwartsUser source) {
+        // We are not setting password in DTO.
+        final UserDto userDto = new UserDto(source.getId(),
+                                            source.getUsername(),
+                                            source.isEnabled(),
+                                            source.getRoles());
+        return userDto;
+    }
+
+}
